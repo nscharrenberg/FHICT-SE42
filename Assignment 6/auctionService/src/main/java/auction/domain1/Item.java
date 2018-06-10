82a7b3ea4f1592aaca42db033c3931e22a2b0c78
Noah Scharrenberg <nscharrenberg@hotmail.com>
2018-06-10 15:14:24 +0200

    auction Service and Client (client broken due to error)

---
+++ Assignment 6/auctionService/src/main/java/auction/domain1/Item.java
@@ -0,0 +1,131 @@
+package auction.domain;
+
+import java.io.Serializable;
+import nl.fontys.util.Money;
+import org.eclipse.persistence.annotations.CascadeOnDelete;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@NamedQueries({
+        @NamedQuery(name = "Item.getAll", query = "SELECT i FROM Item as i"),
+        @NamedQuery(name = "Item.count", query = "SELECT count(i) FROM Item as i"),
+        @NamedQuery(name = "Item.findById", query = "select i from Item as i where i.id = :id"),
+        @NamedQuery(name = "Item.findByDescription", query = "select i from Item as i where i.description = :description"),
+})
+public abstract class Item implements Comparable, Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    private User seller;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "description",
+                    column = @Column(name = "cat_description"))
+    })
+    private Category category;
+
+    private String description;
+
+    @OneToOne(cascade = CascadeType.PERSIST)
+    private Bid highest;
+
+    public Item() {
+    }
+
+    public Item(User seller, Category category, String description) {
+        this.seller = seller;
+        this.category = category;
+        this.description = description;
+        connectToSeller();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public User getSeller() {
+        return seller;
+    }
+    public void setSeller(User user) { this.seller = user; }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Bid getHighestBid() {
+        return highest;
+    }
+
+    public Bid newBid(User buyer, Money amount) {
+        if (highest != null && highest.getAmount().compareTo(amount) >= 0) {
+            return null;
+        }
+        highest = new Bid(buyer, amount, this);
+        return highest;
+    }
+    
+    private void connectToSeller() {
+        seller.addItem(this);
+    }
+
+    public int compareTo(Object arg0) {
+        if(arg0 instanceof Item) {
+            Item oItem = (Item)arg0;
+            return oItem.getHighestBid().getAmount().compareTo(this.getHighestBid().getAmount());
+        }
+
+        return -1;
+    }
+
+    public boolean equals(Object o) {
+        if(o == null) {
+            return false;
+        }
+
+        if(!(o instanceof Item)) {
+            return false;
+        }
+
+        Item other = (Item)o;
+
+        if(this.getSeller() != other.getSeller()) {
+            return false;
+        }
+
+        if(this.getCategory() != other.getCategory()) {
+            return false;
+        }
+
+        if(!this.getDescription().equals(other.getDescription())) {
+            return false;
+        }
+
+        if(this.getHighestBid() != other.getHighestBid()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + Objects.hashCode(this.id);
+        hash = 79 * hash + Objects.hashCode(this.seller);
+        hash = 79 * hash + Objects.hashCode(this.category);
+        hash = 79 * hash + Objects.hashCode(this.description);
+        hash = 79 * hash + Objects.hashCode(this.highest);
+        return hash;
+    }
+   }
---
+++ Assignment 6/auctionService/src/main/java/auction/domain1/User.java
@@ -0,0 +1,63 @@
+package auction.domain;
+
+import java.io.Serializable;
+import org.eclipse.persistence.annotations.CascadeOnDelete;
+
+import javax.persistence.*;
+import java.util.*;
+
+@Entity(name = "users")
+@NamedQueries({
+        @NamedQuery(name = "User.getAll", query = "SELECT u FROM users as u"),
+        @NamedQuery(name = "User.count", query = "SELECT count(u) FROM users as u"),
+        @NamedQuery(name = "User.findByEmail", query = "SELECT u FROM users as u WHERE u.email = :email"),
+})
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @OneToMany(mappedBy = "seller", cascade = CascadeType.PERSIST)
+    private Set<Item> offeredItems;
+
+    public User() {
+        this.offeredItems = new HashSet<>();
+
+    }
+
+    public User(String email) {
+        this.offeredItems = new HashSet<>();
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Iterator<Item> getOfferedItems() {
+        return Collections.unmodifiableSet(this.offeredItems).iterator();
+    }
+
+    public int numerOfOfferedItems() {
+        return Collections.unmodifiableSet(this.offeredItems).size();
+    }
+
+    void addItem(Item item) {
+        this.offeredItems.add(item);
+    }
+}
